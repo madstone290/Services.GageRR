10faa9b6ed559a7fac3340126635c17033b08473
madstone290 <madstone290@gmail.com>
2023-08-21 12:04:38 +0900

    Feat Range method 페이지 추가

--- Services.GageRR.WebApp/Pages/RangeMethod.cshtml.cs
+++ Services.GageRR.WebApp/Pages/RangeMethod.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Services.GageRR.WebApp.Pages
 {
-    public class IndexModel : PageModel
+    public class RangeMethodModel : PageModel
     {
+        [FromQuery]
+        public int Appraiser { get; set; } = 2;
+
+        [FromQuery]
+        public int Part { get; set; } = 5;
+
         public void OnGet()
         {
         }
